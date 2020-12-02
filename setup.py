9e3b5943f772276aacbd62b272903414bb7e83ce
Ilya Lesikov <ilya@lesikov.com>
2020-12-02 22:19:19 +0300

    bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="werf_chart_repo_tasks",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/ilya-lesikov/werf_chart_repo_tasks",
     license="MIT",
 
